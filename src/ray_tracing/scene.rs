8a3743fba42265ebf50725a95d721a94d88d5765
White-Green <43771790+White-Green@users.noreply.github.com>
2021-01-17 15:52:13 +0900

    球の当たり判定をつくった

--- src/ray_tracing/scene.rs
+++ src/ray_tracing/scene.rs
@@ -1,5 +1,9 @@
+use crate::geometry::NormalizedVec3;
+use crate::ray_tracing::Ray;
+
 pub mod camera;
 pub mod object;
 
 trait Collision {
+    fn collision(&self, ray: &Ray) -> Option<(f64, NormalizedVec3<f64>)>;
 }
---
+++ src/ray_tracing/scene/object.rs
@@ -0,0 +1,124 @@
+use crate::geometry::{NormalizedVec3, Vec3};
+use crate::ray_tracing::Ray;
+use crate::ray_tracing::scene::Collision;
+
+#[derive(Clone, Debug)]
+struct Sphere {
+    center: Vec3<f64>,
+    radius: f64,
+}
+
+impl Collision for Sphere {
+    fn collision(&self, ray: &Ray) -> Option<(f64, NormalizedVec3<f64>)> {
+        let d: Vec3<_> = ray.direction.into();
+        let c = self.center - ray.initial;
+        let a = d.squared_len();
+        let half_b = -d.inner_product(c);
+        let c = c.squared_len() - self.radius * self.radius;
+        if half_b * half_b - a * c < 0.0 {
+            None
+        } else {
+            let x = (-half_b - (half_b * half_b - a * c).sqrt()) / a;
+            let x = if x > 0.0 {
+                x
+            } else {
+                (-half_b + (half_b * half_b - a * c).sqrt()) / a
+            };
+            Some((x, (ray.initial + d * x - self.center).normalize()))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::geometry::Vec3;
+    use crate::ray_tracing::Ray;
+    use crate::ray_tracing::scene::Collision;
+    use crate::ray_tracing::scene::object::Sphere;
+
+    #[test]
+    fn sphere_collision_test() {
+        let sphere = Sphere { center: Vec3::new(0f64, 0f64, 0f64), radius: 1.0 };
+        let collision = sphere.collision(&Ray { initial: Vec3::new(2f64, 0f64, 0f64), direction: Vec3::new(-1f64, 0f64, 0f64).normalize() });
+        if let Some((x, normal)) = collision {
+            let normal: Vec3<_> = normal.into();
+            assert!((x - 1.0).abs() < 1e-3);
+            assert!((normal.x() - 1.0).abs() < 1e-3);
+            assert!((normal.y() - 0.0).abs() < 1e-3);
+            assert!((normal.z() - 0.0).abs() < 1e-3);
+        } else {
+            unreachable!()
+        }
+
+        let collision = sphere.collision(&Ray { initial: Vec3::new(0f64, 2f64, 0f64), direction: Vec3::new(0f64, -1f64, 0f64).normalize() });
+        if let Some((x, normal)) = collision {
+            let normal: Vec3<_> = normal.into();
+            assert!((x - 1.0).abs() < 1e-3);
+            assert!((normal.x() - 0.0).abs() < 1e-3);
+            assert!((normal.y() - 1.0).abs() < 1e-3);
+            assert!((normal.z() - 0.0).abs() < 1e-3);
+        } else {
+            unreachable!()
+        }
+
+        let collision = sphere.collision(&Ray { initial: Vec3::new(0f64, 0f64, 2f64), direction: Vec3::new(0f64, 0f64, -1f64).normalize() });
+        if let Some((x, normal)) = collision {
+            let normal: Vec3<_> = normal.into();
+            assert!((x - 1.0).abs() < 1e-3);
+            assert!((normal.x() - 0.0).abs() < 1e-3);
+            assert!((normal.y() - 0.0).abs() < 1e-3);
+            assert!((normal.z() - 1.0).abs() < 1e-3);
+        } else {
+            unreachable!()
+        }
+
+
+        let sphere = Sphere { center: Vec3::new(2f64, 0f64, 0f64), radius: 1.0 };
+        let collision = sphere.collision(&Ray { initial: Vec3::new(0f64, 0f64, 0f64), direction: Vec3::new(1f64, 0f64, 0f64).normalize() });
+        if let Some((x, normal)) = collision {
+            let normal: Vec3<_> = normal.into();
+            assert!((x - 1.0).abs() < 1e-3);
+            assert!((normal.x() + 1.0).abs() < 1e-3);
+            assert!((normal.y() - 0.0).abs() < 1e-3);
+            assert!((normal.z() - 0.0).abs() < 1e-3);
+        } else {
+            unreachable!()
+        }
+
+        let sphere = Sphere { center: Vec3::new(0f64, 2f64, 0f64), radius: 1.0 };
+        let collision = sphere.collision(&Ray { initial: Vec3::new(0f64, 0f64, 0f64), direction: Vec3::new(0f64, 1f64, 0f64).normalize() });
+        if let Some((x, normal)) = collision {
+            let normal: Vec3<_> = normal.into();
+            assert!((x - 1.0).abs() < 1e-3);
+            assert!((normal.x() - 0.0).abs() < 1e-3);
+            assert!((normal.y() + 1.0).abs() < 1e-3);
+            assert!((normal.z() - 0.0).abs() < 1e-3);
+        } else {
+            unreachable!()
+        }
+
+        let sphere = Sphere { center: Vec3::new(0f64, 0f64, 2f64), radius: 1.0 };
+        let collision = sphere.collision(&Ray { initial: Vec3::new(0f64, 0f64, 0f64), direction: Vec3::new(0f64, 0f64, 1f64).normalize() });
+        if let Some((x, normal)) = collision {
+            let normal: Vec3<_> = normal.into();
+            assert!((x - 1.0).abs() < 1e-3);
+            assert!((normal.x() - 0.0).abs() < 1e-3);
+            assert!((normal.y() - 0.0).abs() < 1e-3);
+            assert!((normal.z() + 1.0).abs() < 1e-3);
+        } else {
+            unreachable!()
+        }
+
+        let sphere = Sphere { center: Vec3::new(0f64, 0f64, 0f64), radius: 1.0 };
+        let collision = sphere.collision(&Ray { initial: Vec3::new(0f64, 0f64, 0f64), direction: Vec3::new(1f64, 0f64, 0f64).normalize() });
+        if let Some((x, normal)) = collision {
+            let normal: Vec3<_> = normal.into();
+            assert!((x - 1.0).abs() < 1e-3);
+            assert!((normal.x() - 1.0).abs() < 1e-3);
+            assert!((normal.y() - 0.0).abs() < 1e-3);
+            assert!((normal.z() - 0.0).abs() < 1e-3);
+        } else {
+            unreachable!()
+        }
+    }
+}
